f0b36cd20319e06a2d023803ae3e435818153bd5
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-31 15:04:53 -0300

    fix: Infos content e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -50,14 +50,14 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dr. Sérgio Murilo",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "sergiomuriloadv.com", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "81",
+    firstPart: "99738", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "8798", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -93,7 +93,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "A_definir", // Sem o @
+  instagramProfile: "sergiomuriloadv", // Sem o @
   facebookProfile: "A_Definir",
   linkeDinProfile: "A_Definir",
   x: "A_Definir",
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -80,7 +80,7 @@ export default function Index() {
       {/* <BlogPosts /> */}
       <Faq colorMode={colorMode} />
       {/* <Maps colorMode={colorMode} /> */}
-      <FooterSocial colorMode={colorMode} addres={true} obs={false} />
+      <FooterSocial colorMode={colorMode} addres={true} obs={true} />
       <FloatingWhatsappButton />
       <BackToTopButton />
     </>
